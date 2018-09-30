1db836dc1047c66b9d0e4c86d92d82d88348880d
Richard Obaseki <obasekirichard8@gmail.com>
2018-09-30 14:18:36 +0100

    renders random number to the page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,53 @@
 $(document).ready(function() {
     // Creating Variables
-    var randomNumber = 0;
-    var crystalRandomNumber;
-    var wins = 0;
-    var loss = 0;
+    // This is our current Guess number. Which will be set to zero
+    var randomMatchingNumber = 0;
 
-    // Function to create random number at start of game
-    function generateRandomNumber () {
-        // In this line I calculated the the randomNumber to give me a random number between 19 and 120
-        randomNumber = Math.floor(Math.random() * 120) + 19;
-        // Here I will append the random number to my HTML page
-        $("#random-area").append(randomNumber);
+    // This is the random number that will try to reach
+    var randomNumber = randomNumGen();
+
+    // Our starting variables
+    var wins = 0;
+    var losses = 0;
+    // var counter = 0;
+    var crystal;
+
+    // Function to create a random number between 1 and 12 and return our crystal objects
+    function randomCrystalNum() {
+        return {
+            firstCrystal: {
+                number: Math.round(Math.random() * 12) + 1,
+                url: "assets/images/crystal-1.jpg"
+            },
+            secondCrystal: {
+               number: Math.round(Math.random() * 12) + 1,
+               url: "assets/images/crystal-2.jpg"
+            },
+            thirdcCrystal: {
+               number: Math.round(Math.random() * 12) + 1,
+               url: "assets/images/crystal-3.jpg"
+            },
+            fourthCrystal: {
+               number: Math.round(Math.random() * 12) + 1,
+               url: "assets/images/crystal-4.jpg"
+            }
+        };
     }
 
-    // Function to generate random number for each crystal
-     $("img").one("click", function() {
-        crystalRandomNumber = Math.floor(Math.random() * 9) + 1;
+    // Function  to create a random number between 19 to 120
+    function randomNumGen() {
+        return Math.round(Math.random() * 102) + 19;
+    };
 
-        //  if(!$(this).data('number')){
-
-            // $(this).data('number',crystalRandomNumber);
-
-            // $("#score-area").append(crystalRandomNumber);
-
-        // }
-        $("#score-area").append(crystalRandomNumber);
-    });
-    // calling the generateRandomNumber function
-    generateRandomNumber();
-})
+    // Function that resets the game
+    function newGame() {
+        // Makes the current total number 0
+        randomMatchingNumber = 0;
+        // Generates random crystal values
+        crystal = randomCrystalNum();
+        // Genrates random number that we have to meet and renders it to the page
+        randomNumber = randomNumGen();
+        $("#random-area").text(randomNumber);
+    };
+    newGame();
+});
